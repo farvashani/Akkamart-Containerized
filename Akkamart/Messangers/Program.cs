be22a2dda901aac10993b3982bd601814e2b345b
mkxd771 <mkxd771@yahoo.com>
2019-04-23 04:29:23 +0430

    Add Send Sms to SmsAggregate

--- Akkamart/Messangers/Program.cs
+++ Akkamart/Messangers/Program.cs
@@ -12,7 +12,12 @@ namespace Messanger {
         static void Main (string[] args) {
             var sys = Common.CreateSystem (args[0]);
 
-            sys.ActorOf<SmsManager> ("SmsManager");
+            var smsManager = sys.ActorOf<SmsManager>("SmsManager");
+
+            //Temp Code
+            var smsId = SmsId.New;
+            var cmd = new SendSmsCommand(smsId,"09901359936","Salam");
+            smsManager.Tell(cmd);
 
             Common.WaitForExit ();
             Common.Shutdown (sys);
