621e413b53660fe34ea145835f940b0f7f2eb4c9
hossein farvashani <hossein@hossein-debian.hossein.debian>
2019-04-23 06:47:34 +0430

    Add smsSentResult before Raising sentEvent

--- Akkamart/Messangers/Commands/SendSmsCommand.cs
+++ Akkamart/Messangers/Commands/SendSmsCommand.cs
@@ -1,3 +1,4 @@
+using System;
 using Akkatecture.Commands;
 
 namespace Messangers {
@@ -15,6 +16,19 @@ namespace Messangers {
         public string Recipient { get; set; }
         public string MessageBody { get; set; }
 
+    }
+    public class SmsSentResult : Command<SmsAggregate, SmsId> {
+        public bool IsSucceed { get; private set; }
+        public string Recipient { get; private set; }
+        public string MessageBody { get; private set; }
+        public DateTime SendnigDate { get; private set; }
+
+        public SmsSentResult (SmsId aggregateId, string recipient, string messageBody, DateTime sendnigDate, bool isSucceed = true) : base (aggregateId) {
+
+            this.Recipient = recipient;
+            this.MessageBody = messageBody;
+            this.SendnigDate = sendnigDate;
+        }
     }
 
 }
--- Akkamart/Messangers/SmsAggregate.cs
+++ Akkamart/Messangers/SmsAggregate.cs
@@ -1,37 +1,43 @@
 using System;
+using Akka.Actor;
 using Akkatecture.Aggregates;
 
-namespace Messangers
-{
-    public class SmsAggregate : AggregateRoot<SmsAggregate, SmsId, SmsState>
-    {
+namespace Messangers {
+    public class SmsAggregate : AggregateRoot<SmsAggregate, SmsId, SmsState> {
         public SmsService SmsService { get; set; }
 
-        
-        public SmsAggregate(SmsId id) : base(id)
-        {
+        public SmsAggregate (SmsId id) : base (id) {
             //this.SmsService = smsService;
-            if (SmsService == null)
-            {
+            if (SmsService == null) {
                 var apiKey = "532B6666684D51474166335A5548626F6C686430553264516935733037365745";
-                SmsService = new SmsService(apiKey);
+                SmsService = new SmsService (apiKey);
             }
-            
-            Command<SendSmsCommand>(Execute);
-            
+
+            Command<SendSmsCommand> (Execute);
+            Command<SmsSentResult> (Execute);
+
         }
 
-        private bool Execute(SendSmsCommand cmd)
-        {
-            RequestModel sendParameters = new RequestModel(){
-                receptor = new string[]{cmd.Recipient},
-                sender = new string[]{"10004346"},
-                message = new string[]{cmd.MessageBody}
-            };
-            SmsService.Send(sendParameters);
+        private bool Execute (SmsSentResult cmd) {
+            if (cmd.IsSucceed) {
+                Emit (new SmsSentEvent (cmd.Recipient, cmd.MessageBody, cmd.SendnigDate));
+                return true;
+            } else {
+                return false;
+            }
+        }
 
-            var smsSentEvent = new SmsSentEvent(cmd.Recipient, cmd.MessageBody, DateTime.UtcNow);
-            Emit(smsSentEvent);
+        private bool Execute (SendSmsCommand cmd) {
+            RequestModel sendParameters = new RequestModel () {
+                receptor = new string[] { cmd.Recipient },
+                sender = new string[] { "10004346" },
+                message = new string[] { cmd.MessageBody }
+            };
+            SmsService.Send (sendParameters).ContinueWith (r => {
+                var smsSentEvent = new SmsSentResult (this.Id, cmd.Recipient,
+                    cmd.MessageBody, DateTime.UtcNow);
+            }).PipeTo (Self);
+
             return true;
         }
     }
