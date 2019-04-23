6d33056ec8754172f2c759d047c57d5f9aec26a8
hossein farvashani <hossein@hossein-debian.hossein.debian>
2019-04-23 06:26:47 +0430

    Add ActorServicenames to servicenamesclass

--- Akkamart/Gateway/GatewayActor.cs
+++ Akkamart/Gateway/GatewayActor.cs
@@ -19,6 +19,26 @@ namespace Gateway {
 
             MessangerService = Context.ActorOf (Props.Empty.WithRouter (FromConfig.Instance),
                 MyActorNames.SmsServiceActorname);
+
+            AccountingActor = Context.ActorOf (Props.Empty.WithRouter (FromConfig.Instance),
+                MyActorNames.AccountingActorname);
+            CatalogsActor = Context.ActorOf (Props.Empty.WithRouter (FromConfig.Instance),
+                MyActorNames.CatalogsSActorname);
+            CustomersActor = Context.ActorOf (Props.Empty.WithRouter (FromConfig.Instance),
+                MyActorNames.CustomersActorname);
+            GeoLocatorActor = Context.ActorOf (Props.Empty.WithRouter (FromConfig.Instance),
+                MyActorNames.GeoLocatorActorname);
+
+            InventoryActor = Context.ActorOf (Props.Empty.WithRouter (FromConfig.Instance),
+                MyActorNames.InventoryActorname);
+            MarketingActor = Context.ActorOf (Props.Empty.WithRouter (FromConfig.Instance),
+                MyActorNames.MarketingActorname);
+            OrderingActor = Context.ActorOf (Props.Empty.WithRouter (FromConfig.Instance),
+                MyActorNames.OrderingActorname);
+            PaymentsActor = Context.ActorOf (Props.Empty.WithRouter (FromConfig.Instance),
+                MyActorNames.PaymentsActorname);
+            PointsServiceActor = Context.ActorOf (Props.Empty.WithRouter (FromConfig.Instance),
+                MyActorNames.PointsActorname);
             //var i = 1;
             Receive<ReceiveTimeout> (t => {
                 // _log.Info ("Tick..");
@@ -32,6 +52,15 @@ namespace Gateway {
         public IActorRef MembershipService { get; private set; }
         public IActorRef CredentialService { get; private set; }
         public IActorRef MessangerService { get; private set; }
+        public IActorRef AccountingActor { get; private set; }
+        public IActorRef CatalogsActor { get; private set; }
+        public IActorRef CustomersActor { get; private set; }
+        public IActorRef GeoLocatorActor { get; private set; }
+        public IActorRef InventoryActor { get; private set; }
+        public IActorRef MarketingActor { get; private set; }
+        public IActorRef OrderingActor { get; private set; }
+        public IActorRef PaymentsActor { get; private set; }
+        public IActorRef PointsServiceActor { get; private set; }
 
         protected override void PreStart () {
             _log.Info ("Startup actor starting..");
--- Akkamart/Shared/MyActorNames.cs
+++ Akkamart/Shared/MyActorNames.cs
@@ -6,9 +6,15 @@ namespace Shared {
         public const string SmsServiceActorname = "SmsManager";
         public const string CredentialActorname = "credentials";
         public const string Gateway = "gateway";
+        public static readonly string PointsActorname = "points";
+        public static readonly string PaymentsActorname = "payments";
+        public static readonly string OrderingActorname = "ordering";
+        public static readonly string MarketingActorname = "marketing";
+        public static readonly string InventoryActorname = "inventory";
+        public static readonly string GeoLocatorActorname = "geolocator";
+        public static readonly string CustomersActorname = "customers";
+        public static readonly string CatalogsSActorname = "catalogs";
+        public static readonly string AccountingActorname = "accounting";
 
-        //public const string CredentialActorname = "credential";
-
-        // public static string CredentialActorname { get; set; }
     }
 }
